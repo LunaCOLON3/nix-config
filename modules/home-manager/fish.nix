56b201a00d762113f261ac5574b6b07f07c6a444
LunaCOLON3 </home/luna/.config/sops-nix/secrets/email>
2025-03-16 11:04:08 -0700

    add shell aliases to fish

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -1,11 +1,18 @@
 {...}: {
   programs.fish = {
-  enable = true;
-  shellInit = ''
+    enable = true;
+    shellInit = ''
       set -g theme_color_scheme "catppuccin"
       set -g theme_nerd_fonts "yes"
       set -g theme_title_display_process "yes"
     '';
-  interactiveShellInit = "fastfetch";
+    
+    shellAliases = {
+      vi = "nvim";
+      vim = "nvim";
+      "nix develop" = "nix develop --command fish";
+    };
+
+    interactiveShellInit = "fastfetch";
   };
 }
