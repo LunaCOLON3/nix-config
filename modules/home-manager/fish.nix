e201d00acc3fb2de7c76d17d32691bbe21806301
LunaCOLON3 </home/luna/.config/sops-nix/secrets/email>
2025-03-16 13:48:58 -0700

    change shellAliases to shellAbbrs

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ ... }: {
   programs.fish = {
     enable = true;
     shellInit = ''
@@ -6,12 +6,10 @@
       set -g theme_nerd_fonts "yes"
       set -g theme_title_display_process "yes"
     '';
-    
-    shellAliases = lib.mkMerge [{
+    shellAbbrs = {
       vi = "nvim";
       vim = "nvim";
-      "nix develop" = "nix develop --command fish";
-    }];
+    };
 
     interactiveShellInit = "fastfetch";
   };
