40b1ced58030173ff7a068b7f68176ecae7f4c43
LunaCOLON3 </home/luna/.config/sops-nix/secrets/email>
2025-03-16 22:22:42 -0700

    remove kitty font

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -1,13 +1,9 @@
-{ pkgs, ... }: {
+{ ... }: {
   
   programs.kitty = {
     enable = true;
     settings = {
       background_opacity = "0.85";
-    };
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      package = pkgs.nerd-fonts.jetbrains-mono;
     };
   };
 
