8afe50a9a2c376e31b7c137db385384c1ee65e1a
LunaCOLON3 <lunacolon3@proton.me>
2025-03-17 23:21:07 -0700

    reduce kitty window padding

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       background_opacity = "0.85";
-      window_padding_width = 11;
+      window_padding_width = 5;
     };
     font.name = "JetBrainsMonoNL Nerd Font";
   };
