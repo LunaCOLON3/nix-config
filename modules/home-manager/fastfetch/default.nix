c7bca5faddf1cd5fb3e3439c2cdf7ad3304dfe1c
LunaCOLON3 <lunacolon3@proton.me>
2025-03-17 23:31:02 -0700

    Revert "diojadsio"
    This reverts commit d8d2ab2f647ab5b6aae2c72ce307240bedecb5b9.

--- modules/home-manager/fastfetch/default.nix
+++ modules/home-manager/fastfetch/default.nix
@@ -4,7 +4,7 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        source = "./nixos-ascii.txt";
+        source = "/home/luna/Documents/nix-config/assets/nixos-ascii.txt";
         type = "file";
         color = {
           "1" = "magenta";
