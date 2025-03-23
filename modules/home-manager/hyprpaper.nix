71ba774cc5c40d9195f684c5b7b804fd6e8a260c
LunaCOLON3 <lunacolon3@proton.me>
2025-03-23 13:34:51 -0700

    make wallpaper work with new monitor settings

--- modules/home-manager/hyprpaper.nix
+++ modules/home-manager/hyprpaper.nix
@@ -6,9 +6,9 @@
       preload = "/home/luna/Documents/nix-config/assets/wallpaper.png";
 
       wallpaper = [
-        "HDMI-A-1,/home/luna/Documents/nix-config/assets/wallpaper.png"
-        "DP-2,/home/luna/Documents/nix-config/assets/wallpaper.png"
         "DP-1,/home/luna/Documents/nix-config/assets/wallpaper.png"
+        "DP-2,/home/luna/Documents/nix-config/assets/wallpaper.png"
+        "DP-3,/home/luna/Documents/nix-config/assets/wallpaper.png"
       ];
     };
   };
