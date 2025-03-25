0e747c30f9d6a603b0b1078f594f58260c241ca5
LunaCOLON3 <lunacolon3@proton.me>
2025-03-24 23:05:45 -0700

    fix window rules

--- modules/home-manager/hyprland/rules.nix
+++ modules/home-manager/hyprland/rules.nix
@@ -3,9 +3,9 @@
   wayland.windowManager.hyprland = {
     settings = {
       workspace = [
-        "1, monitor:DP-3, default:true"
-        "2, monitor:DP-2, default:true"
-        "3, monitor:DP-1, default:true"
+        "1, monitor: DP-3, default:true"
+        "2, monitor: DP-2, default:true"
+        "3, monitor: DP-1, default:true"
       ];
     };
   };
