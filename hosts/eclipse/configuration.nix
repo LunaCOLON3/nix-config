407737f1d36cdc856e934f88b61dcb97a06a66d3
LunaCOLON3 <lunacolon3@proton.me>
2025-03-28 16:27:41 -0700

    dhudas

--- hosts/eclipse/configuration.nix
+++ hosts/eclipse/configuration.nix
@@ -68,7 +68,7 @@
       enable = true;
       package = pkgs.kdePackages.sddm;
       setupScript = ''
-         xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1080 --pos 0x0
+         xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1080 --pos 0x0 --primary
       '';
     };
 
