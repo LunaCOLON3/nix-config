67cd3867e94338efaaeb8d451205d6966fbbf4ee
LunaCOLON3 <lunacolon3@proton.me>
2025-03-29 20:44:40 -0700

    add sway

---
+++ modules/home-manager/sway.nix
@@ -0,0 +1,49 @@
+{ pkgs, ... }: {
+
+  home.packages = with pkgs; [
+    swaybg
+    autotiling
+    swayidle
+    swaynotificationcenter
+    kdePackages.polkit-kde-agent-1
+  ];
+
+  wayland.windowManager.sway = {
+    enable = true;
+    package = pkgs.swayfx;
+    
+    checkConfig = false;
+
+    config = {
+      modifier = "Mod4";
+
+      gaps = {
+        outer = 5;
+        inner = 3;
+      };
+
+      output = {
+        "*" = {
+          background = toString ../../assets/wallpaper.png;
+          scale = "1";
+        };
+        "DP-3" = {
+          pos = "0 0";
+        };
+        "DP-2" = {
+          pos = "1920 0";
+        };
+        "DP-1" = {
+          pos = "-1600 180";
+        };
+      };
+
+      startup = [
+        { command = "waybar"; }
+        { command = "systemctl --user start polkit-kde-agent-1.service"; }
+      ];
+
+    };
+  };
+
+}
