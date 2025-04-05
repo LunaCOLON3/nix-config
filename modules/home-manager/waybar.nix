171949c6cb14170429f3a7028af24a7c21b7cc35
LunaCOLON3 <lunacolon3@proton.me>
2025-04-05 12:00:49 -0700

    style waybar

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -2,6 +2,18 @@
 
   programs.waybar = {
     enable = true;
+
+    style = ''
+      * {
+        color: @text;
+        font-family: JetBrainsMonoNL Nerd Font;
+      }
+
+      window#waybar {
+        background-color: shade(@base, 0.9);
+        border: 2px solid alpha(@crust, 0.3);
+      }
+    '';
   };
 
 }
