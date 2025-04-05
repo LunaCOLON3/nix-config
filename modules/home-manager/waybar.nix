16f3563198cfbc1e0582104d1e920347277a7153
LunaCOLON3 <lunacolon3@proton.me>
2025-04-05 13:10:49 -0700

    configure waybar theme
    remove margin from bottom of bar
    
    add more css to waybar
    
    remove weird hover effect from workspaces
    
    make bg transparent for workspaces
    
    add margin to modules
    
    only use margin for right modules

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -6,7 +6,12 @@
     settings = {
       mainBar = {
         modules-left = [ "sway/workspaces" ];
+        modules-center = [ "sway/window" ];
         modules-right = [ "pulseaudio" ];
+
+        margin-top = 8;
+        margin-left = 8;
+        margin-right = 8;
 
         "sway/workspaces" = {
           disable-scroll = true;
@@ -39,6 +44,32 @@
       window#waybar {
         background-color: shade(@base, 0.9);
         border: 2px solid alpha(@crust, 0.3);
+      }
+
+      #workspaces {
+        margin-left: 0;
+      }
+
+      #workspaces button {
+        border: none;
+        background-color: transparent;
+        box-shadow: none;
+        border-radius: 16px;
+        transition: background-color 100ms ease, color 100ms ease;
+
+        min-width: 32px;
+        min-height: 32px;
+        padding: 0;
+        font-weight: normal;
+      }
+
+      #workspaces button:hover {
+        background-image: none;
+        background-color: transparent;
+      }
+
+      .modules-right {
+        margin-right: 8px;
       }
     '';
   };
