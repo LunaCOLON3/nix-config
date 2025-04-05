fe3b699b88a6545ed9b42e8bfe20bc235a169cf4
LunaCOLON3 <lunacolon3@proton.me>
2025-04-05 16:39:12 -0700

    use root path for fastfetch ascii

--- modules/home-manager/fastfetch.nix
+++ modules/home-manager/fastfetch.nix
@@ -1,10 +1,12 @@
-{...}: {
-  programs.fastfetch = {
+{ rootPath, ...}: {
+  programs.fastfetch = let
+    nixos-ascii = rootPath + /assets/nixos-ascii.txt;
+  in {
     enable = true;
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        source = "/home/luna/Documents/nix-config/assets/nixos-ascii.txt";
+        source = nixos-ascii;
         type = "file";
         color = {
           "1" = "magenta";
