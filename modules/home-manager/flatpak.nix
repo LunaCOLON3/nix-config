b8254443427bd525d6189eeeeb1cd01f1c475095
LunaCOLON3 <lunacolon3@proton.me>
2025-04-06 10:56:23 -0700

    set origin for bottles flatpak

--- modules/home-manager/flatpak.nix
+++ modules/home-manager/flatpak.nix
@@ -6,7 +6,7 @@
     enable = true;
     packages = [
       { flatpakref = "https://sober.vinegarhq.org/sober.flatpakref"; sha256 = "1pj8y1xhiwgbnhrr3yr3ybpfis9slrl73i0b1lc9q89vhip6ym2l"; }
-      "com.usebottles.bottles"
+      { appId = "com.usebottles.bottles"; origin = "flathub"; }
     ];
   };
 }
