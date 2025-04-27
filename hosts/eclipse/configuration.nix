853b16b371491d35ba40b4a1041f43c0c10dff57
LunaCOLON3 <lunacolon3@proton.me>
2025-04-27 13:51:25 -0700

    add nm-applet to system packages

--- hosts/eclipse/configuration.nix
+++ hosts/eclipse/configuration.nix
@@ -111,6 +111,7 @@
     kdePackages.partitionmanager
     kdePackages.isoimagewriter
     waywall
+    networkmanagerapplet
     (glfw-wayland-minecraft.overrideAttrs (old: {
           patches = [
             (fetchpatch2 {
