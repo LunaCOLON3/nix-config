fc64b150ccef3ce28df2505ec33eaa963f7662ef
LunaCOLON3 <lunacolon3@proton.me>
2025-04-27 17:32:17 -0700

    fix lualine tabline

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -49,8 +49,6 @@
           ignore_focus = [ "neo-tree" ];
           tabline = {
             lualine_a = [ "buffers" ];
-            lualine_b = [ "branch"];
-            lualine_c = [ "filename"];
             lualine_x = [];
             lualine_y = [];
             lualine_z = [ "tabs"];
