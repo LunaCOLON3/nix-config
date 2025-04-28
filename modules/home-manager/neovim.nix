cf6162e1270c9cf4dec33f779536040dcd9391ea
LunaCOLON3 <lunacolon3@proton.me>
2025-04-27 17:34:09 -0700

    maybe fix this time

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -49,6 +49,8 @@
           ignore_focus = [ "neo-tree" ];
           tabline = {
             lualine_a = [ "buffers" ];
+            lualine_b = [];
+            lualine_c = [];
             lualine_x = [];
             lualine_y = [];
             lualine_z = [ "tabs"];
